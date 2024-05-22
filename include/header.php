5a93bdaf8a2bbc5af6e9c4f1a06646e5eee1d68f
edson45667893 <98639316+edson45667893@users.noreply.github.com>
2024-05-22 18:34:27 +0200

    header fin

--- include/header.php
+++ include/header.php
@@ -2,17 +2,18 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="stylesheet" href="footer-header.css">
     <title>Document</title>
-</head>
-<body> 
-    <header> 
-    <h1 class="header">Blog-Group 4</h1>
-        <h3 class="header">HOME</h3>  
-        
-    
-    
-    <header>
-    
+    </head>
+<body>
+  <header>
+    <div class="header">
+      <nav>
+        <a href="#">Home</a>
+      </nav>
+      <h1>Blog - Group 4</h1>
+    </div>
+  </header>
 </body>
 </html>
