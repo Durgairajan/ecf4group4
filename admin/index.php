a32992128e930f8e25084c9b91fed22ca3f4fff1
nanjimbr <nanji.mendiboure@gmail.com>
2024-05-24 11:25:48 +0200

    changement href articles -> view.php

--- admin/index.php
+++ admin/index.php
@@ -28,7 +28,7 @@ include '../include/config.php';
         <div style="clear: both; margin-bottom: 50px;">
             <div style="width: 100%;">
                 <h3 style="color: blue; margin: 0px;">
-                    <a href="../post.php?id=<?php echo $row['Id_article']; ?>" style="color: blue;"><?php echo htmlspecialchars($row['Titre']); ?></a>
+                    <a href="../view.php?id=<?php echo $row['Id_article']; ?>" style="color: blue;"><?php echo htmlspecialchars($row['Titre']); ?></a>
                 </h3>
                 <small style="margin: 0px; background: blue; color: white; padding: 3px; font-size: 10px;">Category: <?php echo htmlspecialchars($row['category_name']); ?></small>
                 <small style="margin: 0px; background: blue; color: white; padding: 3px; font-size: 10px;">Date: <?php echo htmlspecialchars($row["Date"]); ?></small>
@@ -37,7 +37,7 @@ include '../include/config.php';
             </div>
             
             <p style="font-family: calibri;">
-                <a style="background: blue; padding: 5px; color: white; text-decoration: none;" href="../post.php?id=<?php echo $row['Id_article']; ?>">Read More</a>
+                <a style="background: blue; padding: 5px; color: white; text-decoration: none;" href="../view.php?id=<?php echo $row['Id_article']; ?>">Read More</a>
                 <a style="background: blue; padding: 5px; color: white; text-decoration: none;" href="edit.php?id=<?php echo $row['Id_article']; ?>">Edit Post</a>
                 <a style="background: blue; padding: 5px; color: white; text-decoration: none;" href="delete.php?id=<?php echo $row['Id_article']; ?>">Delete</a>
             </p>
